1a3ba110bf6b04a46d044931f9c78f98596d717d
SLNY1921394421 <1921394421@qq.com>
2017-01-13 17:00:10 +0800

    这是第一次提交！！！

---
+++ src/Demo1.java
@@ -0,0 +1,13 @@
+
+public class Demo1 {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("这是第一次修改！！！");
+
+	}
+
+}
---
+++ src/com/git/Demo2.java
@@ -0,0 +1,12 @@
+package com.git;
+
+public class Demo2 {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("这是git  版本的");
+	}
+}
